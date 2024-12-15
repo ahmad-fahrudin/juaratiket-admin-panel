39ab99d811d814bf0e736c9dd08db068261bb97d
Dimas Ahmad Rizal <fahrudinasevenfold@gmail.com>
2024-12-15 23:36:24 +0700

    creating resource

---
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookingTransactionResource\Pages;
+use App\Filament\Resources\BookingTransactionResource\RelationManagers;
+use App\Models\BookingTransaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookingTransactionResource extends Resource
+{
+    protected static ?string $model = BookingTransaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone_number')
+                    ->tel()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('total_amount')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Toggle::make('is_paid')
+                    ->required(),
+                Forms\Components\TextInput::make('total_participant')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('ticket_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\DatePicker::make('started_at')
+                    ->required(),
+                Forms\Components\TextInput::make('proof')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('booking_trx_id')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('total_amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_paid')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('total_participant')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('ticket_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('started_at')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('proof')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('booking_trx_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookingTransactions::route('/'),
+            'create' => Pages\CreateBookingTransaction::route('/create'),
+            'edit' => Pages\EditBookingTransaction::route('/{record}/edit'),
+        ];
+    }
+}
